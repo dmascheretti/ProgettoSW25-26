4478772f3c44079cf850d70a78e82ceabdffbe66
dmascheretti <167296926+dmascheretti@users.noreply.github.com>
2025-11-18 17:39:11 +0100

    Risolta issue #7
    Accadeva per problemi String-enum dello stato della colonnina. Con enum la mappa non leggeva le colonnine correttamente. Rimesso String poi si vedrà quale è meglio utilizzare.

--- find-n-charge/src/main/java/com/example/models/Colonnina.java
+++ find-n-charge/src/main/java/com/example/models/Colonnina.java
@@ -11,7 +11,7 @@ public class Colonnina {
 	private String id;
 	private String nome;
 	private String tipo;
-	private Stato stato;
+	private String stato;
 	private double latitudine;
 	private double longitudine;
 	private String indirizzo;
@@ -44,11 +44,11 @@ public class Colonnina {
 		this.tipo = tipo;
 	}
 
-	public Stato getStato() {
+	public String getStato() {
 		return stato;
 	}
 
-	public void setStato(Stato stato) {
+	public void setStato(String stato) {
 		this.stato = stato;
 	}
 
