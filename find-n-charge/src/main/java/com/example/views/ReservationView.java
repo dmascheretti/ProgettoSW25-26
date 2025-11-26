20e6de2059e13b064a78f17acf881c0a5e9a27d6
valenari <f.valenari@studenti.unibg.it>
2025-11-26 18:11:13 +0100

    Doppia grid per prenotazioni passate o future in  ReservView

--- find-n-charge/src/main/java/com/example/views/ReservationView.java
+++ find-n-charge/src/main/java/com/example/views/ReservationView.java
@@ -7,6 +7,7 @@ package com.example.views;
 
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -44,18 +45,20 @@ import java.util.concurrent.CompletableFuture;
 
 public class ReservationView extends VerticalLayout {
 
-	private Grid<Prenotazione> reservationGrid = new Grid<>(Prenotazione.class);
+	private Grid<Prenotazione> NewPrenoGrid = new Grid<>(Prenotazione.class, false);
+	private Grid<Prenotazione> OldPrenoGrid = new Grid<>(Prenotazione.class, false);
 	private CompletableFuture<List<Prenotazione>> listaPreno;
 	private final FirebaseService prenotazioniRef;
 	private final UI ui;
 	private QRCode qr;
-
+	
 	/**
 	 * Costruttore che genera la griglia contenente tutte le prenotazioni
 	 * dell'utente
 	 * 
 	 * @param fb Database per accedere ai dati prenotazione
 	 */
+	
 	public ReservationView(FirebaseService fb) {
 		this.ui = UI.getCurrent();
 		this.prenotazioniRef = fb;
@@ -67,78 +70,85 @@ public class ReservationView extends VerticalLayout {
 		titolo.getStyle().set("color", "#008000");
 
 		// Configurazione griglia (visualizzazione dei campi di ogni prenotazione)
-		reservationGrid.setColumns("nomeColonnina", "data", "inizio");
-
-		reservationGrid.addColumn(prenotazione -> {
+		
+		// Prenotazioni attive o future
+		NewPrenoGrid.removeAllColumns();
+		NewPrenoGrid.addColumn(Prenotazione::getNomeColonnina).setHeader("Colonnina").setSortable(true);
+		NewPrenoGrid.addColumn(Prenotazione::getData).setHeader("Data").setSortable(true);
+		NewPrenoGrid.addColumn(Prenotazione::getInizio).setHeader("Inizio").setSortable(true);
+		NewPrenoGrid.addColumn(prenotazione -> {
 			String stato = calcolaStato(prenotazione);
 			return stato;
 		}).setHeader("Stato").setSortable(true);
+		
+		//Prenotazioni passate
+		OldPrenoGrid.removeAllColumns();
+		OldPrenoGrid.addColumn(Prenotazione::getNomeColonnina).setHeader("Colonnina").setSortable(true);
+		OldPrenoGrid.addColumn(Prenotazione::getData).setHeader("Data").setSortable(true);
+		OldPrenoGrid.addColumn(Prenotazione::getInizio).setHeader("Inizio").setSortable(true);
+
 
 		// Aggiunge la colonna per la visualizzazione del QR Code
-		reservationGrid.addComponentColumn(p -> {
-			String stato = calcolaStato(p);
-			if (stato.equals("Passata")) {
-				return null;
-			} else {
-				Button btn = new Button("Visualizza QR");
-				btn.getStyle().set("color", "green").set("text-decoration", "underline").set("background", "none")
-						.set("border", "none");
+		NewPrenoGrid.addComponentColumn(p -> {
+			Button btn = new Button("Visualizza QR");
+			btn.getStyle().set("color", "green").set("text-decoration", "underline").set("background", "none")
+					.set("border", "none");
 
-				btn.addClickListener(event -> {
-					String id = p.getId();
+			btn.addClickListener(event -> {
+				String id = p.getId();
 
-					BufferedImage tempQr = null; // Crea variabile dell'immagine QR Code
+				BufferedImage tempQr = null; // Crea variabile dell'immagine QR Code
+				try {
+					tempQr = QRCode.generaQR(id); // Assegna il valore del QR Code
+				} catch (Exception e) {
+					e.printStackTrace();
+					Notification.show("Errore generazione QR", 3000, Notification.Position.TOP_CENTER).getElement()
+							.getThemeList().add("error");
+					return;
+				}
+
+				final BufferedImage finalQrCode = tempQr;
+
+				Dialog dialog = new Dialog();
+
+				// Da BufferedImage (volatile) a Image (componente Vaadin)
+				StreamResource resource = new StreamResource("qr.png", () -> {
 					try {
-						tempQr = QRCode.generaQR(id); // Assegna il valore del QR Code
-					} catch (Exception e) {
+						ByteArrayOutputStream bos = new ByteArrayOutputStream();
+						ImageIO.write(finalQrCode, "png", bos);
+						return new ByteArrayInputStream(bos.toByteArray());
+					} catch (IOException e) {
 						e.printStackTrace();
-						Notification.show("Errore generazione QR", 3000, Notification.Position.TOP_CENTER).getElement()
-								.getThemeList().add("error");
-						return;
+						return null;
 					}
-
-					final BufferedImage finalQrCode = tempQr;
-
-					Dialog dialog = new Dialog();
-
-					// Da BufferedImage (volatile) a Image (componente Vaadin)
-					StreamResource resource = new StreamResource("qr.png", () -> {
-						try {
-							ByteArrayOutputStream bos = new ByteArrayOutputStream();
-							ImageIO.write(finalQrCode, "png", bos);
-							return new ByteArrayInputStream(bos.toByteArray());
-						} catch (IOException e) {
-							e.printStackTrace();
-							return null;
-						}
-					});
-
-					Image image = new Image(resource, "QR Code");
-					image.setWidth("300px");
-
-					VerticalLayout dialogLayout = new VerticalLayout(image);
-					dialogLayout.setAlignItems(FlexComponent.Alignment.CENTER);
-					dialog.add(dialogLayout);
-
-					H3 header = new H3(" QR Code Prenotazione");
-
-					// Pulsante di chiusura dialog
-					Button closeButton = new Button(VaadinIcon.CLOSE.create(), e -> dialog.close());
-					closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-					closeButton.getStyle().set("align-self", "flex-end");
-					closeButton.getElement().getThemeList().add("success");
-					closeButton.getStyle().set("margin-left", "auto"); // Spinge il pulsante tutto a destra
-
-					dialog.getHeader().add(header, closeButton);
-					dialog.open();
 				});
 
-				return btn;
-			}
+				Image image = new Image(resource, "QR Code");
+				image.setWidth("300px");
+
+				VerticalLayout dialogLayout = new VerticalLayout(image);
+				dialogLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+				dialog.add(dialogLayout);
+
+				H3 header = new H3(" QR Code Prenotazione");
+
+				// Pulsante di chiusura dialog
+				Button closeButton = new Button(VaadinIcon.CLOSE.create(), e -> dialog.close());
+				closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+				closeButton.getStyle().set("align-self", "flex-end");
+				closeButton.getElement().getThemeList().add("success");
+				closeButton.getStyle().set("margin-left", "auto"); // Spinge il pulsante tutto a destra
+
+				dialog.getHeader().add(header, closeButton);
+				dialog.open();
+			});
+
+			return btn;
+		
 		}).setHeader("");
 
 		// bottone per la cancellazione
-		reservationGrid.addComponentColumn(p -> {
+		NewPrenoGrid.addComponentColumn(p -> {
 			String stato = calcolaStato(p);
 			if (stato.equals("Passata")) {
 				return null;
@@ -150,7 +160,13 @@ public class ReservationView extends VerticalLayout {
 				return btn;
 			}
 		});
-		add(titolo, reservationGrid);
+		
+		H4 newP = new H4("Prenotazioni Attive o Future");
+		newP.getStyle().set("color", "#00E000");
+		H4 oldP = new H4("Prenotazioni Passate");
+		oldP.getStyle().set("color", "#00E000");
+		
+		add(titolo, newP, NewPrenoGrid, oldP, OldPrenoGrid);
 
 		/*
 		 * Chiamo funzione da firebaseService che restituisce la lista delle
@@ -162,7 +178,16 @@ public class ReservationView extends VerticalLayout {
 			getUI().ifPresent(ui -> ui.access(() -> {
 
 				// aggiungo la lista alla griglia
-				reservationGrid.setItems(lista);
+				
+				List<Prenotazione> filtrata = lista.stream()
+			            .filter(p -> !calcolaStato(p).equals("Passata"))
+			            .toList();
+				NewPrenoGrid.setItems(filtrata);
+				
+				List<Prenotazione> rimanenti = lista.stream()
+			            .filter(p -> calcolaStato(p).equals("Passata"))
+			            .toList();
+				OldPrenoGrid.setItems(rimanenti);
 
 			}));
 
